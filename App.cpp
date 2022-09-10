bc10a74efd23c442fd63c6b47f18511ba9e1e668
Radu Manea <30615642+radubm1@users.noreply.github.com>
2022-09-10 14:46:56 +0300

    Update App.cpp
    Persistence layer added.

--- App.cpp
+++ App.cpp
@@ -11,11 +11,18 @@ Student create(string s, int a, map<int, string> m) {
 list<Student> write() {
 	list<Student> ls;
 
-	ls.push_back(create("Vasile", 21, { {10, "Math"}, {8,"Chem"} }));
-	ls.push_back(create("Ioana", 19,{ {9, "Math"}, {10,"Chem"} }));
-	ls.push_back(create("Ion", 20, { {8,"Math"},{9,"Chem"} }));
+	ls.push_back(create("Vasile", 21, { {10, "Math"}, {8,"Chem"} , {9,"Bio"} }));
+	ls.push_back(create("Ioana", 19,{ {9, "Math"}, {10,"Chem"}, {8,"Geo"}, {8,"Bio"} }));
+	ls.push_back(create("Rares", 20, { {8,"Math"},{9,"Chem"} }));
+	ls.push_back(create("Ion", 22, { {8,"Math"},{9,"Chem"} }));
+	ls.push_back(create("Matilda", 20, { {8,"Math"},{9,"Chem"} , {10,"Draw"} , {10,"Sport"} }));
+	ls.push_back(create("Clotilde", 18, { {8,"Math"},{9,"Chem"} , {8,"Geo"} }));
+	ls.push_back(create("Mihai", 20, { {8,"Math"},{9,"Chem"}, {9,"Psych"}, {10,"Lit"} }));
+	ls.push_back(create("Gabriel", 24, { {8,"Math"},{9,"Chem"}, {8,"Sport"}, {8,"Lit"}, {8,"Bio"} }));
+	ls.push_back(create("Costica", 19, { {8,"Math"} }));
+	ls.push_back(create("Matei", 20, { {8,"Math"},{9,"Chem"} }));
 
-	cout << "You have successfully added 3 records!" << endl;
+	cout << "You have successfully added 10 records!" << endl;
 
 	return ls;
 }
@@ -36,7 +43,27 @@ void show(list<Student> ls) {
 	ls.clear();
 }
 
+void save(list<Student> ls) {
+	int count = 0;
+	ofstream data, data1;
+	data.open("C:\\Users\\Radu\\Desktop\\file.dat");
+	data1.open("C:\\Users\\Radu\\Desktop\\file1.dat");
+	list<Student>::iterator i;
+	map<int, string>::iterator j;
+
+	for (i = ls.begin();i != ls.end();i++) {
+		data << ++count << "\t" << i->name << "\t" << i->age << endl;
+		for (j = i->marks.begin();j != i->marks.end(); j++) {
+			data1 << j->second << "\t" << j->first << "\t" << count << endl;
+		}
+	}
+	data.close(); data1.close();
+
+}
+
 int main() {
+
+	save(write());
 
 	show(write());
 	
