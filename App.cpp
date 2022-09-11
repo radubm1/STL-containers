cace8dd6dcbbd59f0b9b79a3a6479fab2417577c
Radu Manea <30615642+radubm1@users.noreply.github.com>
2022-09-11 15:51:22 +0300

    Update App.cpp
    Import data from all the extern sources(files) into internal (memory) data model

--- App.cpp
+++ App.cpp
@@ -9,97 +9,89 @@ Student create(string name, int age, map<int, string> mark) {
 }
 
 list<Student> write() {
-	list<Student> ls;
-
-	ls.push_back(create("Vasile", 21, { {10, "Math"}, {8,"Chem"} , {9,"Bio"} }));
-	ls.push_back(create("Ioana", 19,{ {9, "Math"}, {10,"Chem"}, {8,"Geo"}, {8,"Bio"} }));
-	ls.push_back(create("Rares", 20, { {8,"Math"},{9,"Chem"} }));
-	ls.push_back(create("Ion", 22, { {8,"Math"},{9,"Chem"} }));
-	ls.push_back(create("Matilda", 20, { {8,"Math"},{9,"Chem"} , {10,"Draw"} , {10,"Sport"} }));
-	ls.push_back(create("Clotilde", 18, { {8,"Math"},{9,"Chem"} , {8,"Geo"} }));
-	ls.push_back(create("Mihai", 20, { {8,"Math"},{9,"Chem"}, {9,"Psych"}, {10,"Lit"} }));
-	ls.push_back(create("Gabriel", 24, { {8,"Math"},{9,"Chem"}, {8,"Sport"}, {8,"Lit"}, {8,"Bio"} }));
-	ls.push_back(create("Costica", 19, { {8,"Math"} }));
-	ls.push_back(create("Matei", 20, { {8,"Math"},{9,"Chem"} }));
+	c.ls.push_back(create("Vasile", 21, { {10, "Math"}, {8,"Chem"} , {9,"Bio"} }));
+	c.ls.push_back(create("Ioana", 19,{ {9, "Math"}, {10,"Chem"}, {8,"Geo"}, {8,"Bio"} }));
+	c.ls.push_back(create("Rares", 20, { {8,"Math"},{9,"Chem"} }));
+	c.ls.push_back(create("Ion", 22, { {8,"Math"},{9,"Chem"} }));
+	c.ls.push_back(create("Matilda", 20, { {8,"Math"},{9,"Chem"} , {10,"Draw"} , {10,"Sport"} }));
+	c.ls.push_back(create("Clotilde", 18, { {8,"Math"},{9,"Chem"} , {8,"Geo"} }));
+	c.ls.push_back(create("Mihai", 20, { {8,"Math"},{9,"Chem"}, {9,"Psych"}, {10,"Lit"} }));
+	c.ls.push_back(create("Gabriel", 24, { {8,"Math"},{9,"Chem"}, {8,"Sport"}, {8,"Lit"}, {8,"Bio"} }));
+	c.ls.push_back(create("Costica", 19, { {8,"Math"} }));
+	c.ls.push_back(create("Matei", 20, { {8,"Math"},{9,"Chem"} }));
 
 	cout << "You have successfully added 10 records!" << endl;
 
-	return ls;
+	return c.ls;
 }
 
 void show(list<Student> ls) {
-	list<Student>::iterator i;
-	map<int, string>::iterator j;
 
-	for (i = ls.begin();i != ls.end();i++) {
-		cout << i->name << endl;
-		cout << i->age << endl;
-		for (j = i->marks.begin();j != i->marks.end(); j++) {
-			cout << j->second << endl;
-			cout << j->first << endl;
+	for (c._i = ls.begin();c._i != ls.end();c._i++) {
+		cout << c._i->name << endl;
+		cout << c._i->age << endl;
+		for (c._j = c._i->marks.begin();c._j != c._i->marks.end(); c._j++) {
+			cout << c._j->second << endl;
+			cout << c._j->first << endl;
 		}
-		i->marks.clear();
+		c._i->marks.clear();
 	}
-	ls.clear();
+	c.ls.clear();
 }
 
 void save(list<Student> ls) {
-	int count = 0;
-	ofstream data, data1;
-	data.open("C:\\Users\\Radu\\Desktop\\file.dat");
-	data1.open("C:\\Users\\Radu\\Desktop\\file1.dat");
-	list<Student>::iterator i;
-	map<int, string>::iterator j;
 
-	for (i = ls.begin();i != ls.end();i++) {
-		data << ++count << "\t" << i->name << "\t" << i->age << endl;
-		for (j = i->marks.begin();j != i->marks.end(); j++) {
-			data1 << j->second << "\t" << j->first << "\t" << count << endl;
+	int count = 0;
+	x1._out.open(str);
+	x2._out.open(str1);
+
+	for (c._i = ls.begin();c._i != ls.end();c._i++) {
+		x1._out << ++count << "\t" << c._i->name << "\t" <<c._i->age << endl;
+		for (c._j = c._i->marks.begin();c._j != c._i->marks.end(); c._j++) {
+			x2._out << c._j->second << "\t" << c._j->first << "\t" << count << endl;
 		}
 	}
-	data.close(); data1.close();
+	x1._out.close(); x2._out.close();
 
+	c.ls.clear();
 }
 
 list<Student> import() {
-	Student s = Student();
-	list<Student> ls;
-	list<Student>::iterator it;
-	ifstream data, data1;
-	data.open("C:\\Users\\Radu\\Desktop\\file.dat");
-	data1.open("C:\\Users\\Radu\\Desktop\\file1.dat");
-	string line, field;
-	const char separator = '\n';
-	const char separator1 = '\t';
-	while (getline(data,line,separator)) {
-		stringstream streamData(line);
-		int cnt = 1;
-		while (getline(streamData, field, separator1)) {
-			switch (cnt) {
-			case 2:
-				s.name = field;
-				break;
-			case 3:
-				s.age = stoi(field);
-				cnt = 0;
-				break;
-			default:
-				break;
-			}
-			cnt++;
+
+	int cnt = 0;
+	x1._in.open(str);
+	while (getline(x1._in,x1.line)) {
+		stringstream sData(x1.line);
+
+		sData >> cnt;
+		sData >> s.name;
+		sData >> s.age;
+
+		x2._in.open(str1);// To do...
+		while(getline(x2._in, x2.line)) {
+			stringstream sData(x2.line);
+			int key=0, mcnt = 0;
+			string val;
+
+			sData >> val;
+			sData >> key;
+			sData >> mcnt;
+
+			if (cnt==mcnt) s.marks.insert(pair<int,string>(key, val));
 		}
-		ls.push_back(s);
+		c.ls.push_back(s);
 		s = Student();
+		x2._in.close();
 	}
-	//for (it = ls.begin();it != ls.end();it++)
-		//cout << it->age << "\t" << it->name << endl;
-	return ls;
+	x1._in.close(); 
+	cout << "You've loaded " << cnt << " records into memory!" << endl;
+	return c.ls;
 }
 
 int main() {
 
 	//save(write());
-
+	//--show(write());
 	show(import());
 	
 	return 0;
