d57dff4e22dde1b3fe21aa73644dc35b77dddd5e
Radu Manea <30615642+radubm1@users.noreply.github.com>
2022-09-11 15:54:41 +0300

    Update Student.h
    Contains all the referenced types, instead of global instances.

--- Student.h
+++ Student.h
@@ -3,6 +3,11 @@
 #include <map>
 #include <list>
 #include <iterator>
+#include <fstream>
+#include <sstream>
+
+constexpr auto str = "C:\\Users\\Radu\\Desktop\\file.dat";
+constexpr auto str1 = "C:\\Users\\Radu\\Desktop\\file1.dat";
 
 using namespace std;
 
@@ -10,4 +15,16 @@ struct Student {
 	string name;
 	int age = 0;
 	map<int, string> marks;
-};
+} s;
+
+struct Collection {
+	list<Student> ls;
+	list<Student>::iterator _i;
+	map<int, string>::iterator _j;
+} c;
+
+struct IO {
+	ifstream _in;
+	ofstream _out;
+	string line, field;
+} x1,x2;
