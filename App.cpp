1387f3b6c9b40909aef4fd2f3b78d33991cb9f9c
Radu Manea <30615642+radubm1@users.noreply.github.com>
2022-09-17 13:34:19 +0300

    Update App.cpp
    Using constructor and local variables

--- App.cpp
+++ App.cpp
@@ -1,35 +1,31 @@
 #include<Student.h>
 
-Student create(string name, int age, map<string, int> mark) {
-	Student inst = Student();
-	inst.age = age;
-	inst.name = name;
-	inst.marks = mark;
-	return inst;
-}
-
 list<Student> write() {
-	c.ls.push_back(create("Vasile", 21, { {"Math", 10}, {"Chem", 9} , {"Bio",10} }));
-	c.ls.push_back(create("Ioana", 19,{ {"Math", 9}, {"Chem", 8}, {"Geo", 10}, {"Bio", 8} }));
-	c.ls.push_back(create("Rares", 20, { {"Math", 10},{"Chem", 10} }));
+	list<Student> ls;
+	ls.push_back(Student("Vasile", 21, { {"Math", 10}, {"Chem", 9} , {"Bio",10} }));
+	ls.push_back(Student("Ioana", 19,{ {"Math", 9}, {"Chem", 8}, {"Geo", 10}, {"Bio", 8} }));
+	ls.push_back(Student("Rares", 20, { {"Math", 10},{"Chem", 10} }));
 
 	cout << "You have successfully added 3 records!" << endl;
 
-	return c.ls;
+	return ls;
 }
 
 void show(list<Student> ls) {
 
-	for (c._i = ls.begin();c._i != ls.end();c._i++) {
-		cout << c._i->name << endl;
-		cout << c._i->age << endl;
-		for (c._j = c._i->marks.begin();c._j != c._i->marks.end(); c._j++) {
-			cout << c._j->first << endl;
-			cout << c._j->second << endl;
+	ls.sort();
+
+	for (list<Student>::iterator _i = ls.begin();_i != ls.end();_i++) {
+		cout << _i->name << endl;
+		cout << _i->age << endl;
+		cout << _i->avg << endl;
+		for (map<string, int>::iterator _j = _i->marks.begin();_j != _i->marks.end(); _j++) {
+			cout << _j->first << endl;
+			cout << _j->second << endl;
 		}
-		c._i->marks.clear();
+		_i->marks.clear();
 	}
-	c.ls.clear();
+	ls.clear();
 }
 
 void save(list<Student> ls) {
@@ -38,27 +34,28 @@ void save(list<Student> ls) {
 	x1._out.open(str);
 	x2._out.open(str1);
 
-	for (c._i = ls.begin();c._i != ls.end();c._i++) {
-		x1._out << ++count << "\t" << c._i->name << "\t" <<c._i->age << endl;
-		for (c._j = c._i->marks.begin();c._j != c._i->marks.end(); c._j++) {
-			x2._out << c._j->first << "\t" << c._j->second << "\t" << count << endl;
+	for (list<Student>::iterator _i = ls.begin();_i != ls.end();_i++) {
+		x1._out << ++count << "\t" << _i->name << "\t" <<_i->age << "\t" << _i->avg << endl;
+		for (map<string, int>::iterator _j = _i->marks.begin();_j != _i->marks.end(); _j++) {
+			x2._out << _j->first << "\t" << _j->second << "\t" << count << endl;
 		}
 	}
 	x1._out.close(); x2._out.close();
 
-	c.ls.clear();
+	ls.clear();
 }
 
 list<Student> import() {
-
+	list<Student> ls;
 	int cnt = 0;
 	x1._in.open(str);
 	while (getline(x1._in,x1.line)) {
 		stringstream sData(x1.line);
-
+		string name; int age;
+		map<string, int> marks;
 		sData >> cnt;
-		sData >> s.name;
-		sData >> s.age;
+		sData >> name;
+		sData >> age;
 
 		x2._in.open(str1);// To do...
 		while(getline(x2._in, x2.line)) {
@@ -70,20 +67,19 @@ list<Student> import() {
 			sData >> val;
 			sData >> mcnt;
 
-			if (cnt==mcnt) s.marks.insert(pair<string, int>(key, val));
+			if (cnt==mcnt) marks.insert(pair<string, int>(key, val));
 		}
-		c.ls.push_back(s);
-		s = Student();
+		ls.push_back(Student(name, age, marks));
 		x2._in.close();
 	}
 	x1._in.close(); 
 	cout << "You've loaded " << cnt << " records into memory!" << endl;
-	return c.ls;
+	return ls;
 }
 
 int main() {
 
-	//save(write());
+	save(write());
 	//show(write());
 	show(import());
 	
